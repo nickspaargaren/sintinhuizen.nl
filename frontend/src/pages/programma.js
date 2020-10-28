ae637272917c61653530589a597e8f0753aaa396
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2020-10-28 21:17:26 +0100

    Programma styling en favicon

--- frontend/src/pages/programma.js
+++ frontend/src/pages/programma.js
@@ -1,17 +1,20 @@
 import React from "react"
 import Layout from "../components/layout/layout.js";
+import styles from '../styles/programma.module.css';
 
 export default function Programma({data}) {
   
   return <>
           <Layout title="Programma title" description="Programma description">
-            Programma
-            {data.tijdslots.nodes.map(item =>
-              <div key={item.id}>
-                {item.tijd}
-                {item.titel}
-              </div>
-            )}
+            <h1>Programma</h1>
+            <table className={styles.programma} cellspacing="0">
+              {data.tijdslots.nodes.map(item =>
+                <tr key={item.id}>
+                  <td>{item.tijd}</td>
+                  <td>{item.titel}</td>
+                </tr>
+              )}
+              </table>
             </Layout>
         </>
 }
