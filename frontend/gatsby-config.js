1132f61035a7db02bf6cc9cb5575e63982545a26
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2020-10-28 18:43:15 +0100

    Sanity geïnstalleerd

---
+++ frontend/gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'az5d5e06',
+        dataset: 'production',
+        watchMode: true,
+        token: process.env.GATSBY_SANITY_TOKEN,
+      }
+    }
+  ],
+}
