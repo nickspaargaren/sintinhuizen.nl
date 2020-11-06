48a44d131b8d33c91ee0f1501579d6467388590b
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2020-11-06 08:25:38 +0100

    Footer

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -4,6 +4,8 @@ import Header from "../components/header/Header.js";
 import Programma from "../components/programma/Programma.js";
 import Sponsoren from "../components/sponsoren/Sponsoren.js";
 
+import mijter from "../assets/images/mijter.png";
+
 export default function Home() {
   return <>
           <Layout title="Home" description="Informatie over de aankomst Sint Nicolaas in de werkhaven van het Nautisch Kwartier!">
@@ -15,11 +17,17 @@ export default function Home() {
               </div>
             </section>
             
-            <section>
+            <section className="sponsoren">
               <Sponsoren/>
             </section>
 
-            <footer>Footer</footer>
+            <footer>
+              <p>De jaarlijkse intocht van de Sint in Huizen wordt georganiseerd door de St. Intocht Sint Nicolaas Huizen<br/>
+              Bestuur: Roeland Bijlard, Hans Borremans, Laura Toele, Jurgen van Wessel. Gelieerd bestuurslid: Eric Dinkla</p>
+              <p>Bankrek. NL63 RABO 0344 4312 74 | Tel. woordvoerder 0651 98 92 18 | <a href="mailto:info@sintinhuizen.nl">info@sintinhuizen.nl</a> | postadres: Slangekruid 6, 1273 VM Huizen | KvK 75336707</p>
+              <p><img className="knopHome" alt="mijter" src={mijter} style={{height: '35px', width: '30px', verticalAlign: 'middle'}} /></p>
+              <p style={{opacity: '0.8'}}>sintinhuizen.nl is gesponsord door <a rel="noopener noreferrer" href="https://www.cmspecialist.nl" target="_blank">CM Specialist</a></p>
+            </footer>
 
           </Layout>
         </>
