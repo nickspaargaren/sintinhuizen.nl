bab79a463bd39e6c253b8b2fd8df170da3cbae3a
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2020-11-06 08:25:38 +0100

    Footer

--- frontend/src/components/sponsoren/Sponsoren.js
+++ frontend/src/components/sponsoren/Sponsoren.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby";
 import {Trail} from 'react-spring/renderprops';
-import Img from "gatsby-image"
+// import Img from "gatsby-image"
 
 import styles from './sponsoren.module.css';
 
@@ -31,7 +31,7 @@ const Sponsoren = () => {
           <Trail items={sponsoren.nodes} keys={item => item.id} from={{opacity: 0, transform: 'scale(0.5)'}} to={{opacity: 1, transform: 'scale(1)'}}>
             {item => props => 
               <div style={props} key={item.id} className={styles.item}>
-                <a  rel="noopener noreferrer" href={item.bedrijfswebsite} target="_blank">
+                <a rel="noopener noreferrer" href={item.bedrijfswebsite} target="_blank">
                   <img src={item.logo.asset.url} alt={item.bedrijfsnaam} />
                 </a>
               </div>
--- frontend/src/styles/GlobalStyles.js
+++ frontend/src/styles/GlobalStyles.js
@@ -15,11 +15,18 @@ body {
   background-repeat: repeat;
 }
 
+a {color: inherit;}
+p {
+  margin: 0 0 1em 0;
+}
+
 main section {
   min-height: 100vh;
   display: flex;
   justify-content: center;
 }
+
+main section.sponsoren {background: #fff; min-height: 0;}
 
 main section > div {margin: auto; padding: 20px;}
 
@@ -31,6 +38,8 @@ div[class*="grid"] img {max-width: 100%; height: auto !important;}
 .grid-3x {grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));}
 .grid-4x {grid-template-columns: repeat(auto-fit, minmax(270px, 1fr));}
 /* Einde Grid Templates */
+
+footer {background: #b72322; text-align: center; padding: 20px;}
 
 `;
 
