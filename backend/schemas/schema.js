45c3eddec63f7d25def234321feb1757c3feccb4
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2020-11-06 09:24:23 +0100

    fotos setup

--- backend/schemas/schema.js
+++ backend/schemas/schema.js
@@ -5,6 +5,7 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 import programma from './programma.js'
+import fotos from './fotos.js'
 import sponsoren from './sponsoren.js'
 
 // Then we give our schema to the builder and provide the result to Sanity
@@ -13,5 +14,5 @@ export default createSchema({
   name: 'default',
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([programma, sponsoren])
+  types: schemaTypes.concat([programma, sponsoren, fotos])
 })
