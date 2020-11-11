bccf1ff1e28cbf2e4fe6f1f38462be37b97971d9
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2020-11-11 10:54:56 +0100

    Header, koppen en foto's styling

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -10,12 +10,15 @@ const Header = () => {
 
   return (
         <div className={`grid-2x ${styles.header}`}>
-          <div style={{display: 'flex', flexDirection: 'column'}}>
-            <img style={{margin: '0 auto auto 0'}} src={logo} alt="Logo" />
-            <h1>
-            <strong>Aankomst</strong> <br/><span>Sint Nicolaas</span> <br/><small>in</small> <strong>Huizen</strong>
-            </h1>
-            <p>Je kunt dit zien op zaterdag 14 november vanaf 14:00 uur op deze site</p>
+          <div className={styles.headertekst}>
+            
+            <div>
+              <h1>
+              <strong>Aankomst</strong> <br/><span>Sint Nicolaas</span> <br/><small>in</small> <strong>Huizen</strong>
+              </h1>
+              <h2>Je kunt dit zien op zaterdag <span><strong>14 november</strong></span> vanaf <strong>14:00</strong> uur op deze site</h2>
+            </div>
+            <img src={logo} alt="Logo" />
           </div>
           
           <div style={{display: 'flex'}}>
