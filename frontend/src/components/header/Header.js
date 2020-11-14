711473290f970574fe3c56899f3507819a31ce65
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2020-11-14 13:58:20 +0100

    14 november (#6)
    * Eerste opzet voor de 14e
    
    * Knop header responsive fix
    
    * Responsive en besproken aanpassingen
    
    * Teksten en foto
    
    * Foto's popup fix een titel aanpassing

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "react-scroll";
 
 import styles from './header.module.css';
 import logo from "../../assets/images/logo.svg";
@@ -16,45 +17,18 @@ const Header = () => {
               <h1>
               <strong>Aankomst</strong> <br/><span>Sint Nicolaas</span> <br/><small>in</small> <strong>Huizen</strong>
               </h1>
-              <h2>Je kunt dit zien op zaterdag <span><strong>14 november</strong></span> vanaf <strong>14:00</strong> uur op deze site</h2>
+
+              <Link className={styles.knop} to="aankomst" smooth={true}>Bekijk de aankomst op 14 november!</Link>
+
             </div>
             <img src={logo} alt="Logo" style={{width: '300px', height: '195px'}} loading="lazy" />
           </div>
           
-          <div style={{display: 'flex'}}>
-            <img style={{marginTop: 'auto', width: '100%', maxHeight: '80vh'}} src={sinterklaas} alt="Sinterklaas" width="415px" height="440px" loading="lazy" />
+          <div className={styles.illustratie}>
+            <img src={sinterklaas} alt="Sinterklaas" width="415px" height="440px" loading="lazy" />
           </div>
         </div>
   )
 }
 
 export default Header
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
