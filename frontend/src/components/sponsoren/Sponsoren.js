70e1e9eb48dcbdfb87ccb004df7f6e07d1f4f11f
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2021-01-13 19:35:19 +0100

    React spring verangen

--- frontend/src/components/sponsoren/Sponsoren.js
+++ frontend/src/components/sponsoren/Sponsoren.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby";
-import {Trail} from 'react-spring/renderprops';
 import Img from "gatsby-image"
 
 import styles from './sponsoren.module.css';
@@ -36,13 +35,11 @@ const Sponsoren = () => {
   )
   return (
         <div className={styles.sponsoren}>
-          <Trail items={sponsoren.nodes} keys={item => item.id} from={{opacity: 0, transform: 'scale(0.5)'}} to={{opacity: 1, transform: 'scale(1)'}}>
-            {item => props => 
-              <a style={props} key={item.id} className={styles.item} rel="noopener noreferrer" href={item.bedrijfswebsite} target="_blank">
+            {sponsoren.nodes.map((item) =>
+              <a key={item.id} className={styles.item} rel="noopener noreferrer" href={item.bedrijfswebsite} target="_blank">
                 <Img fluid={item.logo.asset.fluid} loading="lazy" />
               </a>
-            }
-          </Trail>
+            )}
         </div>
   )
 }
