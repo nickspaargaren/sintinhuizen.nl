af3bdb85da48ab7bc9fd87cbf4c083539d106c38
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2021-01-29 18:13:43 +0100

    Fotoalbum blader functionaliteit

--- frontend/src/components/fotos-2020/Fotos-2020.js
+++ frontend/src/components/fotos-2020/Fotos-2020.js
@@ -1,6 +1,7 @@
-import React from "react"
+import React, { useState } from 'react'; 
 import { useStaticQuery, graphql } from "gatsby";
-import ModalImage from "react-modal-image";
+import Img from "gatsby-image"
+import FsLightbox from 'fslightbox-react';
 
 import styles from './fotos.module.css';
 
@@ -13,10 +14,17 @@ const Fotos = () => {
           id
           childImageSharp {
             fluid(maxWidth: 1200) {
+              base64
+              aspectRatio
               src
+              srcSet
+              sizes
             }
             fixed(width: 270, height: 200) {
               src
+              srcWebp
+              srcSetWebp
+              srcSet
             }
           }
         }
@@ -25,14 +33,20 @@ const Fotos = () => {
     `
   )
 
+  const [toggler, setToggler] = useState(false);
+
   return (
       <div className={styles.fotos}>
         <h3 style={{textAlign: 'center'}}>Foto's van de aankomst op <strong>14 november</strong> 2020</h3>
         <p style={{textAlign: 'center', margin: '-15px 0 40px'}}>Klik op de foto voor een vergroting</p>
         <div>
           {fotos.nodes.map((item) =>
-            <ModalImage key={item.id} className={styles.item} small={item.childImageSharp.fixed.src} large={item.childImageSharp.fluid.src} />
+            <div key={item.id} className={styles.item} aria-hidden="true" onClick={() => setToggler(!toggler)}>
+              <Img fixed={item.childImageSharp.fixed}/>
+            </div>
           )}
+
+          <FsLightbox toggler={toggler} sources={fotos.nodes.map((item) => item.childImageSharp.fluid.src )} /> 
         </div>
       </div>
   )
