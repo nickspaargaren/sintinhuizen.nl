2972c093c394cdf251e1cf04c5f5fc93b816c79f
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2021-03-02 23:02:58 +0100

    React 17/gatsby 3.0 (#7)
    * Updates plus styled components
    
    Co-authored-by: Nick Spaargaren <nickspaargaren@users.noreply.github.com>

--- frontend/src/components/fotos-2020/Fotos-2020.js
+++ frontend/src/components/fotos-2020/Fotos-2020.js
@@ -4,7 +4,38 @@ import { Gallery, Item } from 'react-photoswipe-gallery'
 import 'photoswipe/dist/photoswipe.css'
 import 'photoswipe/dist/default-skin/default-skin.css'
 
-import styles from './fotos.module.css';
+import styled from 'styled-components';
+
+const StyledFotos = styled.div`
+  margin: auto 0; width: 100%;
+  ::after {clear: both; content: ""; float: none; display: block;}
+   .item {
+    box-shadow: 0 10px 39px -5px rgba(0,0,0,.2);
+    float: left;
+    line-height: 0;
+    margin: 5px;
+    position: relative;
+    text-align: center;
+    overflow: hidden;
+    width: calc(16.666% - 10px);
+    box-sizing: border-box;
+    background: rgba(255,255,255,.7);
+    transform: rotate(-2deg);
+    padding: 3px;
+    transition: .3s all ease;
+    cursor: pointer;
+  }
+   .item img {width: 100%;}
+
+
+  @media screen and (max-width: 1024px) {
+    .item {width: calc(25% - 10px);}
+  }
+
+  @media screen and (max-width: 768px) {
+    .item {width: calc(33.333% - 10px);}
+  }
+`;
 
 const Fotos = () => {
   const { fotos } = useStaticQuery(
@@ -35,13 +66,13 @@ const Fotos = () => {
   )
 
   return (
-      <div className={styles.fotos}>
+      <StyledFotos>
         <h3 style={{textAlign: 'center'}}>Foto's van de aankomst op <strong>14 november</strong> 2020</h3>
         <p style={{textAlign: 'center', margin: '-15px 0 40px'}}>Klik op de foto voor een vergroting</p>
         <div>
         <Gallery>
           {fotos.nodes.map((item) =>
-            <div key={item.id} className={styles.item}>
+            <div key={item.id} className="item">
               <Item
                 original={item.childImageSharp.fluid.src}
                 thumbnail={item.childImageSharp.fluid.src}
@@ -56,7 +87,7 @@ const Fotos = () => {
           )}
         </Gallery>
         </div>
-      </div>
+      </StyledFotos>
   )
 }
 
--- frontend/src/components/programma/Programma.js
+++ frontend/src/components/programma/Programma.js
@@ -1,7 +1,41 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import styles from './programma.module.css';
+import styled from 'styled-components';
 import { FaRegClock } from 'react-icons/fa';
+
+const StyledProgramma = styled.div`
+
+  margin: auto;
+  padding: 0;
+  box-sizing: border-box;
+  text-align: left;
+
+
+.row {
+  display: grid;
+  grid-template-columns: 100px 1fr;
+  color: #505050;
+  font-size: 15px;
+  background: #fff;
+}
+.row div {padding: 20px;}
+
+
+.row div:first-child {
+  white-space: nowrap;
+  font-weight: 700;
+  text-transform: uppercase;
+  font-size: 13px;
+  background: #e2a531;
+  color: #fff;
+}
+
+.row div:first-child svg {margin-right: 8px;}
+
+.row:nth-child(even) {
+  background: #fff8f8;
+}
+`;
 
 const Programma = () => {
   const { tijdslots } = useStaticQuery(
@@ -18,14 +52,14 @@ const Programma = () => {
     `
   )
   return (
-        <div className={styles.programma}>
+        <StyledProgramma>
           {tijdslots.nodes.map((item) =>
-              <div key={item.id} className={styles.row}>
+              <div key={item.id} className="row">
                 <div><FaRegClock/>{item.tijd}</div>
                 <div>{item.titel}</div>
               </div>
           )}
-        </div>
+        </StyledProgramma>
   )
 }
 
