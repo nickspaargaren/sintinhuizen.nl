78ef77fbeeb7ab55424c97f4bc089e1016a09127
Nick Spaargaren <nickspaargaren@protonmail.com>
2021-03-23 22:37:11 +0100

    Gatsby source Sanity 7 & GatsbyImage (#9)
    Co-authored-by: Nick Spaargaren <nickspaargaren@users.noreply.github.com>

--- frontend/src/components/sponsoren/Sponsoren.js
+++ frontend/src/components/sponsoren/Sponsoren.js
@@ -1,21 +1,14 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby";
-import Img from "gatsby-image"
+import { GatsbyImage } from "gatsby-plugin-image"
 
 import styled from 'styled-components';
 
 const StyledSponsoren = styled.div`
   display: flex; margin: auto 0; width: 100%;
   a.item {display: flex; flex: 1; width: 100%; padding: 0 5px; position: relative;}
-  a.item > div {width: 100%; margin: auto 0;}
-  a.item > div img {
-    max-height: 80px;
-    width: auto !important;
-    max-width: 100%;
-    margin: auto;
-    right: 0;
-    bottom: 0;
-  }
+  a.item > div {width: 100%; margin: auto 0; max-height: 80px;}
+  a.item > div img {object-fit: contain !important;}
 `;
 
 const Sponsoren = () => {
@@ -26,18 +19,11 @@ const Sponsoren = () => {
         nodes {
           logo {
             asset {
-              fluid {
-                aspectRatio
-                sizes
-                base64
-                src
-                srcSet
-                srcSetWebp
-                srcWebp
-              }
+              gatsbyImageData(height: 80)
             }
           }
           id
+          bedrijfsnaam
           bedrijfswebsite
         }
       }
@@ -49,7 +35,7 @@ const Sponsoren = () => {
         <StyledSponsoren>
             {sponsoren.nodes.map((item) =>
               <a key={item.id} className="item" rel="noopener noreferrer" href={item.bedrijfswebsite} target="_blank">
-                <Img fluid={item.logo.asset.fluid} />
+                <GatsbyImage image={item.logo.asset.gatsbyImageData} alt={item.bedrijfsnaam} />
               </a>
             )}
         </StyledSponsoren>
