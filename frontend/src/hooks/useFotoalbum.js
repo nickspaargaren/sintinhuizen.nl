8fd8f159078c915dbd9e00f20bb53ce0cd81e92c
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2021-04-10 12:32:27 +0200

    Custom hook voor fotoalbumdata

---
+++ frontend/src/hooks/useFotoalbum.js
@@ -0,0 +1,37 @@
+import { graphql, useStaticQuery } from "gatsby"
+
+const FotoalbumData = () => {
+  const { fotos2020, fotos2019 } = useStaticQuery(graphql`
+    query {
+
+      fotos2020: allFile(filter: {relativeDirectory: {eq: "fotos-2020"}}, sort: {fields: relativePath}) {
+        nodes {
+          id
+          relativePath
+          publicURL
+          childImageSharp {
+            gatsbyImageData(width: 270, height: 200)
+          }
+        }
+      }
+
+      fotos2019: allFile(filter: {relativeDirectory: {eq: "fotos-2019"}}, sort: {fields: relativePath}) {
+        nodes {
+          id
+          relativePath
+          publicURL
+          childImageSharp {
+            gatsbyImageData(width: 270, height: 200)
+          }
+        }
+      }
+    }
+    `
+  )
+  return {
+    fotos2020,
+    fotos2019
+  }
+}
+
+export default FotoalbumData
