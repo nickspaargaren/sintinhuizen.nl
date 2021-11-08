2408deb6070f01b64feaf56b0e96a9088d5ffe3c
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2021-11-08 18:16:51 +0100

    Add page verkeer en parkeren

---
+++ frontend/src/components/Button.tsx
@@ -0,0 +1,39 @@
+import { Link } from 'gatsby';
+import React from 'react';
+import styled from 'styled-components';
+
+import { ButtonProps } from '../types';
+
+const StyledButton = styled.a`
+  background-color: #c32c2b;
+  border-radius: 3px;
+  box-shadow: 0px 2px 0px rgba(0,0,0,.15);
+  color: #fff;
+  cursor: pointer;
+  display: inline-block;
+  font-family: 'Alfa Slab One';
+  letter-spacing: .05em;
+  line-height: normal;
+  padding: 10px 13px;
+  text-align: center;
+  text-decoration: none;
+  transition: 0.3s all ease;
+
+  &:hover {
+    background: #f1cb19;
+  }
+
+  @media screen and (max-width: 768px) {
+      display: block;
+  }
+`;
+
+const Button = ({
+  title, url,
+}: ButtonProps) => (
+  <StyledButton as={Link} to={url} className="button">
+    {title}
+  </StyledButton>
+);
+
+export default Button;
