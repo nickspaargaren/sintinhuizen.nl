f8e8642ab2539db806d29fc370a35358de8606af
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2021-11-10 10:43:55 +0100

    Moved Sponsoren to Footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,22 +1,29 @@
 import { StaticImage } from 'gatsby-plugin-image';
 import React, { ReactElement } from 'react';
 
+import Sponsoren from './Sponsoren';
+
 const Footer = (): ReactElement => (
-  <footer>
-    <p>
-      De jaarlijkse intocht van de Sint in Huizen wordt georganiseerd door de St. Intocht Sint Nicolaas Huizen
-      <br />
-      Bestuur: Roeland Bijlard, Hans Borremans, Laura Toele, Jurgen van Wessel. Adviseur: Eric Dinkla
-    </p>
-    <p>
-      Bankrek. NL63 RABO 0344 4312 74 | Tel. woordvoerder 0651 98 92 18 |
-      {' '}
-      <a href="mailto:info@sintinhuizen.nl">info@sintinhuizen.nl</a>
-      {' '}
-      | postadres: Slangekruid 6, 1273 VM Huizen | KvK 75336707
-    </p>
-    <StaticImage className="knopHome" alt="mijter" src="../assets/images/mijter.png" style={{ height: '35px', width: '30px', verticalAlign: 'middle' }} />
-  </footer>
+  <>
+    <section className="sponsoren">
+      <Sponsoren />
+    </section>
+    <footer>
+      <p>
+        De jaarlijkse intocht van de Sint in Huizen wordt georganiseerd door de St. Intocht Sint Nicolaas Huizen
+        <br />
+        Bestuur: Roeland Bijlard, Hans Borremans, Laura Toele, Jurgen van Wessel. Adviseur: Eric Dinkla
+      </p>
+      <p>
+        Bankrek. NL63 RABO 0344 4312 74 | Tel. woordvoerder 0651 98 92 18 |
+        {' '}
+        <a href="mailto:info@sintinhuizen.nl">info@sintinhuizen.nl</a>
+        {' '}
+        | postadres: Slangekruid 6, 1273 VM Huizen | KvK 75336707
+      </p>
+      <StaticImage className="knopHome" alt="mijter" src="../assets/images/mijter.png" style={{ height: '35px', width: '30px', verticalAlign: 'middle' }} />
+    </footer>
+  </>
 );
 
 export default Footer;
