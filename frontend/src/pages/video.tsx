0d8fdc9d4aa7321300496efbe00a682f6ff9c1d2
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2021-11-28 19:51:14 +0100

    Knop tekst aanpassigen "Terug naar de website"

--- frontend/src/pages/video.tsx
+++ frontend/src/pages/video.tsx
@@ -10,7 +10,7 @@ const Video = (): ReactElement => (
     <section>
       <div>
 
-        <div style={{ textAlign: 'right' }}><Button title="Home" url="/" /></div>
+        <div style={{ textAlign: 'right' }}><Button title="Terug naar de website" url="/" /></div>
 
         <VimeoVideo title="Stichting Intocht Sint Nicolaas Huizen Intocht 2021" url="https://player.vimeo.com/video/650779183?h=8770fcab97&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" />
       </div>
