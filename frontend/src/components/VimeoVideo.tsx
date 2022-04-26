469eefb6cf48e642ae84d2f8f93caf0c38da285e
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2022-04-26 19:22:39 +0200

    Update VimeoVideo with styled components

--- frontend/src/components/VimeoVideo.tsx
+++ frontend/src/components/VimeoVideo.tsx
@@ -1,4 +1,19 @@
 import React, { ReactElement } from 'react';
+import styled from 'styled-components';
+
+const StyledVimeoVideo = styled.div`
+  padding-top: 56.25%;
+  position: relative;
+
+  iframe {
+    height: 100%;
+    left: 0;
+    max-height: calc( 100vh - 200px );
+    position: absolute;
+    top: 0;
+    width: 100%;
+  }
+`;
 
 type VimeoVideoProps = {
   title: string,
@@ -6,25 +21,14 @@ type VimeoVideoProps = {
 }
 
 const VimeoVideo = ({ title, url }: VimeoVideoProps): ReactElement => (
-  <>
-    <div style={{ padding: '56.25% 0 0 0', position: 'relative' }}>
-      <iframe
-        src={url}
-        allow="autoplay; fullscreen; picture-in-picture"
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%',
-          height: '100%',
-          maxHeight: 'calc( 100vh - 200px )',
-        }}
-        frameBorder="0"
-        title={title}
-      />
-    </div>
-    <script src="https://player.vimeo.com/api/player.js" />
-  </>
+  <StyledVimeoVideo>
+    <iframe
+      src={url}
+      allow="autoplay; fullscreen; picture-in-picture"
+      frameBorder="0"
+      title={title}
+    />
+  </StyledVimeoVideo>
 );
 
 export default VimeoVideo;
