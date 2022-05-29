7c4fca35e51b73b46a6bcce4ea46066243a8b3dd
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2022-05-29 12:28:41 +0200

    Add cypress test workflow (#23)

--- frontend/cypress/integration/fotoalbum.spec.js
+++ frontend/cypress/integration/fotoalbum.spec.js
@@ -6,15 +6,17 @@ describe("Fotoalbum", () => {
   });
 
   it("Can open and navigate the images in the Fotoalbum", () => {
+
+    cy.wait(1000);
     cy.get("[data-cy=fotoalbum]").first().find('.item').first().click();
 
-    cy.wait(500);
+    cy.wait(1000);
     cy.get(".pswp__button--arrow--next").click();
 
-    cy.wait(500);
+    cy.wait(1000);
     cy.get(".pswp__button--arrow--next").click();
 
-    cy.wait(500);
+    cy.wait(1000);
     cy.get("[aria-label=Close]").click();
   });
 });
