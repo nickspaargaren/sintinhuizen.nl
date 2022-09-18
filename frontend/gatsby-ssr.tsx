c0d91b5620b26953954c1ec9ba4fbfdf0c53acc4
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2022-09-18 12:10:44 +0200

    Add proxy for Plausible Analytics

--- frontend/gatsby-ssr.tsx
+++ frontend/gatsby-ssr.tsx
@@ -6,7 +6,7 @@ export const onRenderBody: GatsbySSR["onRenderBody"] = (
 ) => {
   setPostBodyComponents(
     [
-      <script key="plausible" defer data-domain="sintinhuizen.nl" src="https://plausible.io/js/plausible.js"></script>,
+      <script key="stats" defer data-domain="sintinhuizen.nl" data-api="/stats/api/event" src="/stats/js/script.js"></script>,
     ],
   );
 };
