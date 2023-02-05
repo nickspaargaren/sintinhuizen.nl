fd3408dfb62993c2bbb5c51ad3517054759e1d8e
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2023-02-05 09:59:53 +0100

    Update SSR file with lint fix

--- frontend/gatsby-ssr.tsx
+++ frontend/gatsby-ssr.tsx
@@ -1,15 +1,15 @@
-import type { GatsbySSR } from "gatsby";
-import React from 'react'
+import type { GatsbySSR } from 'gatsby';
+import React from 'react';
 
-export const onRenderBody: GatsbySSR["onRenderBody"] = (
+export const onRenderBody: GatsbySSR['onRenderBody'] = (
   { setHtmlAttributes, setPostBodyComponents },
 ) => {
   setHtmlAttributes({
-    lang: "nl",
-  }),
+    lang: 'nl',
+  });
   setPostBodyComponents(
     [
-      <script key="stats" defer data-domain="sintinhuizen.nl" data-api="/stats/api/event" src="/stats/js/script.js"></script>,
+      <script key="stats" defer data-domain="sintinhuizen.nl" data-api="/stats/api/event" src="/stats/js/script.js" />,
     ],
   );
 };
