b3fe1ee4cd8265384c784b64918414877b1d5844
Nick Spaargaren <20847106+nickspaargaren@users.noreply.github.com>
2024-06-12 17:29:31 +0200

    Add photos of last year (2023) (#55)

--- frontend/src/hooks/usePhotoGallery.tsx
+++ frontend/src/hooks/usePhotoGallery.tsx
@@ -1,47 +1,66 @@
-import { graphql, useStaticQuery } from 'gatsby';
+import { graphql, useStaticQuery } from "gatsby";
 
 const PhotoGalleryData = () => {
-  const {
-    photos2022, drawings,
-  } = useStaticQuery<Queries.photoGalleryQuery>(graphql`
-    query photoGallery {
-      photos2022: allFile(filter: {relativeDirectory: {eq: "photos-2022"}}, sort: { relativePath: ASC }) {
-        nodes {
-          id
-          relativePath
-          publicURL
-          childImageSharp {
-            gatsbyImageData(width: 270, height: 200)
-            original {
-              height
-              width
+  const { photos2023, photos2022, drawings } =
+    useStaticQuery<Queries.photoGalleryQuery>(graphql`
+      query photoGallery {
+        photos2023: allFile(
+          filter: { relativeDirectory: { eq: "photos-2023" } }
+          sort: { relativePath: ASC }
+        ) {
+          nodes {
+            id
+            relativePath
+            publicURL
+            childImageSharp {
+              gatsbyImageData(width: 270, height: 200)
+              original {
+                height
+                width
+              }
+            }
+          }
+        }
+
+        photos2022: allFile(
+          filter: { relativeDirectory: { eq: "photos-2022" } }
+          sort: { relativePath: ASC }
+        ) {
+          nodes {
+            id
+            relativePath
+            publicURL
+            childImageSharp {
+              gatsbyImageData(width: 270, height: 200)
+              original {
+                height
+                width
+              }
+            }
+          }
+        }
+
+        drawings: allFile(
+          filter: { relativeDirectory: { eq: "drawings" } }
+          sort: { relativePath: ASC }
+        ) {
+          nodes {
+            id
+            relativePath
+            publicURL
+            childImageSharp {
+              gatsbyImageData(width: 270, height: 200)
+              original {
+                height
+                width
+              }
             }
           }
         }
       }
-
-      drawings: allFile(filter: {relativeDirectory: {eq: "drawings"}}, sort: { relativePath: ASC }) {
-        nodes {
-          id
-          relativePath
-          publicURL
-          childImageSharp {
-            gatsbyImageData(width: 270, height: 200)
-            original {
-              height
-              width
-            }
-          }
-        }
-      }
-
-    }
     `);
 
-  return [
-    photos2022.nodes,
-    drawings.nodes,
-  ];
+  return [ photos2023.nodes, photos2022.nodes, drawings.nodes ];
 };
 
 export default PhotoGalleryData;
