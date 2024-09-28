e22863dc31af6de65057ac6d713a7139986302ab
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2024-09-28 17:49:48 +0200

    Preload fonts to reduce CLS

--- frontend/gatsby-ssr.tsx
+++ frontend/gatsby-ssr.tsx
@@ -1,10 +1,29 @@
+import alfaSlabOneWoff2 from "@fontsource/alfa-slab-one/files/alfa-slab-one-latin-400-normal.woff?url";
+import courgetteWoff2 from "@fontsource/courgette/files/courgette-latin-400-normal.woff2?url";
 import type { GatsbySSR } from "gatsby";
 import React from "react";
 
 export const onRenderBody: GatsbySSR["onRenderBody"] = ({
+  setHeadComponents,
   setHtmlAttributes,
   setPostBodyComponents,
 }) => {
+  setHeadComponents([
+    <link
+      rel="preload"
+      as="font"
+      type="font/woff2"
+      href={alfaSlabOneWoff2}
+      crossOrigin="anonymous"
+    />,
+    <link
+      rel="preload"
+      as="font"
+      type="font/woff2"
+      href={courgetteWoff2}
+      crossOrigin="anonymous"
+    />,
+  ]);
   setHtmlAttributes({
     lang: "nl",
   });
