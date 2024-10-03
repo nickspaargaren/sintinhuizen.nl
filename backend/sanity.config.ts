a88cee0184872de5e4e5350b05a68523d8ec360d
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2024-10-03 20:49:36 +0200

    Update Sanity language to dutch

--- backend/sanity.config.ts
+++ backend/sanity.config.ts
@@ -1,6 +1,7 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {schemaTypes} from './schemas'
+import {nlNLLocale} from '@sanity/locale-nl-nl'
 
 export default defineConfig({
   name: 'default',
@@ -9,7 +10,7 @@ export default defineConfig({
   projectId: 'az5d5e06',
   dataset: 'production',
 
-  plugins: [structureTool()],
+  plugins: [structureTool(), nlNLLocale(),],
 
   schema: {
     types: schemaTypes,
