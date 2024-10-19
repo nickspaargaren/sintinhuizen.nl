2b827f8dc445aabee142cc362eb86d6a31d5aead
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2024-10-19 14:06:58 +0200

    Remove Layout component

--- frontend/src/components/Layout.tsx
+++
@@ -1,7 +0,0 @@
-import React, { PropsWithChildren, ReactElement } from "react";
-
-const Layout = ({ children }: PropsWithChildren): ReactElement => (
-  <main>{children}</main>
-);
-
-export default Layout;
