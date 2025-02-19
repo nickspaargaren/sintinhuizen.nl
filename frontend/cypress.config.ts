68cd1652d53bba7c66877eea015c40433b354795
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2025-02-19 11:02:56 +0100

    Add html validator e2e test

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "cypress";
+import htmlvalidate from "cypress-html-validate/plugin";
 
 export default defineConfig({
   e2e: {
+    setupNodeEvents(on) {
+      htmlvalidate.install(on);
+    },
     supportFile: false,
     baseUrl: "http://localhost:3000/",
   },
