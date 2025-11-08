2877d796d77cb3003070bdce088cdcfbc78a2d65
Nick Spaargaren <20847106+nickspaargaren@users.noreply.github.com>
2025-11-08 15:47:41 +0100

    Update autoUpdates sanity configuration (#346)

--- backend/sanity.cli.ts
+++ backend/sanity.cli.ts
@@ -3,7 +3,9 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'az5d5e06',
-    dataset: 'production'
+    dataset: 'production',
   },
-  autoUpdates: false
+  deployment: {
+    autoUpdates: false,
+  },
 })
