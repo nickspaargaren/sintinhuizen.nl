98c9092f986cbb168aeb8ded639385f6addcf53f
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2026-07-25 12:33:21 +0200

    Update typescript package constraints to v7

--- yarn.config.cjs
+++ yarn.config.cjs
@@ -32,7 +32,7 @@ const enforceConsistentTypeScriptVersion = (Yarn) => {
     if (workspace.manifest.name === "sintinhuizen.nl") {
       continue;
     }
-    workspace.set("devDependencies.typescript", `^5.9.3`);
+    workspace.set("devDependencies.typescript", `^7.0.2`);
   }
 };
 
